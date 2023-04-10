a41fcd3136c7b1f7919f3657cc81bea40a550011
your_username <putraa998877@gmail.com>
2023-04-11 06:07:06 +0700

    last

--- Latihan 9/model.php
+++ Latihan 9/model.php
@@ -5,7 +5,7 @@ class model {
     public function getData()
     {
         return array(
-            new book('One Piece','Eichiro Oda','Gramedia','2020'),
+            new book('One Piece vl 1080','Eichiro Oda','Gramedia','2020'),
             new book('Pemrograman Web Laravel','Bagoes','Gramedia','2022'),
             new book('Pemrograman Python','Agus','Gramedia','2021'),
             new book('Pemrograman Framework','Putra','Gramedia','2023')
